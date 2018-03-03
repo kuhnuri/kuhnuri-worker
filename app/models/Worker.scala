752ab9001d94dd64e3f0500747b37e18a379289a
Jarno Elovirta <jarno@elovirta.com>
2018-03-03 15:53:58 +0200

    Add support for BCrypt #3

--- app/models/Worker.scala
+++ app/models/Worker.scala
@@ -6,15 +6,16 @@ import play.api.libs.functional.syntax._
 import play.api.libs.json._
 import models.Task.uriWrites
 
-case class Register(id: String, uri: URI)
+case class Register(id: String, password: String, uri: URI)
 
 object Register {
 
 //  def parse(id: String, uri: String) = Register(id, new URI(uri))
 
   implicit val registerWrites: Writes[Register] = (
-      (__ \ "id").write[String] and
+    (__ \ "id").write[String] and
+      (__ \ "password").write[String] and
       (__ \ "uri").write[URI]
-    ) (unlift(Register.unapply _))
+  )(unlift(Register.unapply _))
 
 }
