479b09b062a7e5c0cb6467caf8b63981b1dab2d8
Jarno Elovirta <jarno@elovirta.com>
2018-03-13 17:42:41 +0200

    Add DITA-OT plugin libraries to classpath

--- docker/run.sh
+++ docker/run.sh
@@ -4,6 +4,8 @@ CLASSPATH=conf/
 for i in lib/*.jar; do
     CLASSPATH=$CLASSPATH:$i
 done
+export DITA_HOME=$PWD
+source /opt/app/config/env.sh
 
 if [ -f /opt/app/RUNNING_PID ]; then
     rm /opt/app/RUNNING_PID
