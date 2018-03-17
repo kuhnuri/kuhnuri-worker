5f81829da480dc07b978f098cbfc1b647300c59a
Jarno Elovirta <jarno@elovirta.com>
2018-03-17 14:41:59 +0200

    Add JSON reader for Task

--- app/models/Task.scala
+++ app/models/Task.scala
@@ -1,10 +1,11 @@
 package models
 
-import java.net.URI
+import java.net.{URI, URISyntaxException}
 
 import play.api.libs.functional.syntax._
 import play.api.libs.json._
 import models.Job.jobWrites
+import models.Job.jobReads
 
 sealed case class Task(input: URI, output: URI, job: Job)
 
@@ -16,4 +17,16 @@ object Task {
       (JsPath \ "output").write[URI] and
       (JsPath \ "job").write[Job]
     ) (unlift(Task.unapply _))
+
+  implicit val uriReads = Reads[URI](j => try {
+      JsSuccess(new URI(j.as[JsString].value))
+    } catch {
+      case e: URISyntaxException  => JsError(e.toString)
+    })
+
+  implicit val taskReads: Reads[Task] = (
+    (JsPath \ "input").read[URI] and
+      (JsPath \ "output").read[URI] and
+      (JsPath \ "job").read[Job]
+    ) (Task.apply _)
 }
