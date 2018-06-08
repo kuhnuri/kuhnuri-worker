ee5639c31eabd6a8e25f38d3bc83cd336c302673
Jarno Elovirta <jarno@elovirta.com>
2018-06-08 11:54:26 +0300

    Add abstact base worker

--- app/services/DitaOtWorker.scala
+++ app/services/DitaOtWorker.scala
@@ -7,6 +7,7 @@ import javax.inject.Inject
 import models.{StatusString, Task, Work}
 import org.dita.dost.{Processor, ProcessorFactory}
 import play.Environment
+import play.api.libs.ws.WSClient
 import play.api.{Configuration, Logger}
 import services.Utils.{format, parse}
 
@@ -14,71 +15,72 @@ import scala.concurrent.{ExecutionContext, Future}
 import scala.util.{Failure, Success, Try}
 
 class DitaOtWorker @Inject()(implicit context: ExecutionContext,
+                             ws: WSClient,
                              configuration: Configuration,
-                             environment: Environment) extends Worker {
+                             environment: Environment) extends BaseWorker() {
 
-  private val logger = Logger(this.getClass)
+//  private val logger = Logger(this.getClass)
 
   private val ditaDir = environment.rootPath()
   private val processorFactory = ProcessorFactory.newInstance(ditaDir)
-  private val baseTemp = new File(configuration.get[String]("worker.temp"))
+//  private val baseTemp = new File(configuration.get[String]("worker.temp"))
   processorFactory.setBaseTempDir(baseTemp)
 
   protected val cacheListener = new CacheListener()
-  var stopHook: Option[() => Unit] = None
+//  var stopHook: Option[() => Unit] = None
 
-  private def download(job: Task): Future[Try[Work]] = Future {
-    logger.debug(s"Download: " + job)
-    try {
-      val input = new URI(job.input)
-      logger.info(s"Input URI ${input} scheme ${input.getScheme}")
-      input.getScheme match {
-        case "file" | "http" | "https" if !input.getPath.endsWith("/") => {
-          logger.info(s"Read source directly from " + job.input)
-          Success(Work(new URI(job.input), getProcessOutputDir(job), job))
-        }
-        case "jar" => {
-          val (srcUri, fileUri) = parse(input)
-          val tmpIn = new File(srcUri)
-          val tmpOut = new File(baseTemp, job.id + File.separator + "input")
-          srcUri.getScheme match {
-            case "file" if !srcUri.getPath.endsWith("/") => {
-              logger.info(s"Unzip ${srcUri} ZIP to ${tmpOut.toURI}")
-              Utils.unzip(tmpIn, tmpOut)
-              val inputUri = tmpOut.toURI.resolve(fileUri)
-              logger.info(s"Use processing input " + inputUri)
-              Success(Work(inputUri, getProcessOutputDir(job), job))
-            }
-            //            case "http" | "https" => {
-            //              // Download file
-            //              // Unzip
-            //            }
-            case _ =>
-              throw new IllegalArgumentException(s"Download target ${srcUri} not supported")
-          }
-        }
-        case scheme =>
-          logger.warn(s"Input URI scheme ${scheme} not supported");
-          Failure(new ProcessorException(new IllegalArgumentException(s"Input URI scheme ${scheme} not supported"), job))
-      }
-    } catch {
-      case e: Exception =>
-//        e.printStackTrace();
-        Failure(new ProcessorException(e, job))
-    }
-  }
+//  private def download(job: Task): Future[Try[Work]] = Future {
+//    logger.debug(s"Download: " + job)
+//    try {
+//      val input = new URI(job.input)
+//      logger.info(s"Input URI ${input} scheme ${input.getScheme}")
+//      input.getScheme match {
+//        case "file" | "http" | "https" if !input.getPath.endsWith("/") => {
+//          logger.info(s"Read source directly from " + job.input)
+//          Success(Work(new URI(job.input), getProcessOutputDir(job), job))
+//        }
+//        case "jar" => {
+//          val (srcUri, fileUri) = parse(input)
+//          val tmpIn = new File(srcUri)
+//          val tmpOut = new File(baseTemp, job.id + File.separator + "input")
+//          srcUri.getScheme match {
+//            case "file" if !srcUri.getPath.endsWith("/") => {
+//              logger.info(s"Unzip ${srcUri} ZIP to ${tmpOut.toURI}")
+//              Utils.unzip(tmpIn, tmpOut)
+//              val inputUri = tmpOut.toURI.resolve(fileUri)
+//              logger.info(s"Use processing input " + inputUri)
+//              Success(Work(inputUri, getProcessOutputDir(job), job))
+//            }
+//            //            case "http" | "https" => {
+//            //              // Download file
+//            //              // Unzip
+//            //            }
+//            case _ =>
+//              throw new IllegalArgumentException(s"Download target ${srcUri} not supported")
+//          }
+//        }
+//        case scheme =>
+//          logger.warn(s"Input URI scheme ${scheme} not supported");
+//          Failure(new ProcessorException(new IllegalArgumentException(s"Input URI scheme ${scheme} not supported"), job))
+//      }
+//    } catch {
+//      case e: Exception =>
+////        e.printStackTrace();
+//        Failure(new ProcessorException(e, job))
+//    }
+//  }
 
-  private def getProcessOutputDir(job: Task): URI = {
-    val out = new URI(job.output)
-    out.getScheme match {
-      case "file" if out.getPath.endsWith("/") => {
-        out
-      }
-      case _ => {
-        new File(baseTemp, job.id + File.separator + "output").toURI
-      }
-    }
-  }
+//  private def getProcessOutputDir(job: Task): URI = {
+//    val out = new URI(job.output)
+//    out.getScheme match {
+//      case "file" if out.getPath.endsWith("/") => {
+//        out
+//      }
+//      case _ => {
+//        new File(baseTemp, job.id + File.separator + "output").toURI
+//      }
+//    }
+//  }
 
   private def runDitaOt(jobTry: Try[Work]): Future[Try[Work]] = Future {
     logger.debug(s"Process: " + jobTry)
@@ -137,46 +139,51 @@ class DitaOtWorker @Inject()(implicit context: ExecutionContext,
     processor
   }
 
-  private def upload(tryTask: Try[Work]): Future[Try[Work]] = Future {
-    logger.debug(s"Upload: " + tryTask)
-    tryTask match {
-      case Success(task) => try {
-        val output = new URI(task.task.output)
-        output.getScheme match {
-          case "file" if output.getPath.endsWith("/") => {
-            logger.info(s"Already generated output to ${task.output} directory")
-            if (output != task.output) {
-              throw new IllegalArgumentException(s"Output directory ${task.output} should match ${output}")
-            }
-            Success(task)
-          }
-          case "jar" => {
-            val (srcUri, _) = parse(output)
-            srcUri.getScheme match {
-              case "file" if !srcUri.getPath.endsWith("/") => {
-                logger.info(s"ZIP generated output from ${task.output} to ${srcUri}")
-                Utils.zipDir(new File(task.output), new File(srcUri))
-                Success(task)
-              }
-              //              case "http" | "https" => {
-              //                // ZIP output directory
-              //                // Upload ZIP
-              //              }
-              case _ =>
-                throw new IllegalArgumentException(s"Upload target ${srcUri} not supported")
-            }
-          }
-          case _ =>
-            throw new IllegalArgumentException(s"Upload target ${output} not supported")
-        }
-      } catch {
-        case e: Exception =>
-          //          e.printStackTrace();
-          Failure(new ProcessorException(e, task.task))
-      }
-      case f => f
-    }
-  }
+//  private def upload(tryTask: Try[Work]): Future[Try[Work]] = Future {
+//    logger.debug(s"Upload: " + tryTask)
+//    tryTask match {
+//      case Success(task) => try {
+//        val output = new URI(task.task.output)
+//        output.getScheme match {
+//          case "file" if output.getPath.endsWith("/") => {
+//            logger.info(s"Already generated output to ${task.output} directory")
+//            if (output != task.output) {
+//              throw new IllegalArgumentException(s"Output directory ${task.output} should match ${output}")
+//            }
+//            Success(task)
+//          }
+//          case "jar" => {
+//            val (srcUri, _) = parse(output)
+//            srcUri.getScheme match {
+//              case "file" if !srcUri.getPath.endsWith("/") => {
+//                logger.info(s"ZIP generated output from ${task.output} to ${srcUri}")
+//                Utils.zipDir(new File(task.output), new File(srcUri))
+//                Success(task)
+//              }
+//              //              case "http" | "https" => {
+//              //                // ZIP output directory
+//              //                // Upload ZIP
+//              //              }
+//              case _ =>
+//                throw new IllegalArgumentException(s"Upload target ${srcUri} not supported")
+//            }
+//          }
+////          case "s3" if output.getPath().endsWith(".zip") => {
+//            // TODO
+//            // ZIP output directory
+//            // upload ZIP to S3
+////          }
+//          case _ =>
+//            throw new IllegalArgumentException(s"Upload target ${output} not supported")
+//        }
+//      } catch {
+//        case e: Exception =>
+//          //          e.printStackTrace();
+//          Failure(new ProcessorException(e, task.task))
+//      }
+//      case f => f
+//    }
+//  }
 
   override def process(tryJob: Try[Task]): Future[Try[Work]] = tryJob match {
     case Success(job) => {
@@ -212,7 +219,7 @@ class DitaOtWorker @Inject()(implicit context: ExecutionContext,
   override def log(offset: Int) = cacheListener.messages.slice(offset, cacheListener.messages.size)
     .map(msg => msg.msg)
 
-  override def addStopHook(callback: () => Unit): Unit = {
-    stopHook = Some(callback)
-  }
+//  override def addStopHook(callback: () => Unit): Unit = {
+//    stopHook = Some(callback)
+//  }
 }
---
+++ common/app/services/BaseWorker.scala
@@ -0,0 +1,207 @@
+package services
+
+import java.io.File
+import java.net.URI
+import java.nio.file.StandardCopyOption.REPLACE_EXISTING
+import java.nio.file.{Files, Paths}
+
+import com.amazonaws.{AmazonClientException, AmazonServiceException}
+import com.amazonaws.regions.{Region, Regions}
+import com.amazonaws.services.s3.model.{GetObjectRequest, PutObjectRequest}
+import com.amazonaws.services.s3.{AmazonS3, AmazonS3Client}
+import javax.inject.Inject
+import models.{StatusString, Task, Work}
+import play.Environment
+import play.api.libs.ws.WSClient
+import play.api.{Configuration, Logger}
+import services.Utils.format
+
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success, Try}
+
+abstract class BaseWorker @Inject()(implicit context: ExecutionContext,
+                                    ws: WSClient,
+                                    configuration: Configuration,
+                                    environment: Environment) extends Worker {
+
+  protected val logger = Logger(this.getClass)
+
+  protected val baseTemp = new File(configuration.get[String]("worker.temp"))
+
+  private val s3: AmazonS3 = new AmazonS3Client()
+  s3.setRegion(Region.getRegion(Regions.EU_CENTRAL_1))
+
+  var stopHook: Option[() => Unit] = None
+
+//  override def process(tryJob: Try[Task]): Future[Try[Work]] = tryJob match {
+//    case Success(job) => {
+//      logger.info(s"Got job ${job.id}")
+//      for {
+//        src <- download(job)
+//        otRes <- runDitaOt(src)
+//        res <- upload(otRes)
+//      } yield res
+//    }
+//    case Failure(e) => Future(Failure(e))
+//  }
+
+  private def getProcessOutputDir(job: Task): URI = URI.create(job.input + ".zip")
+
+  def download(job: Task): Future[Try[Work]] = Future {
+    logger.debug(s"Download: " + job)
+    try {
+      val input = URI.create(job.input)
+      logger.info(s"Input URI ${input} scheme ${input.getScheme}")
+      input.getScheme match {
+        case "file" if !input.getPath.endsWith("/") => {
+          logger.info(s"Read source directly from " + job.input)
+          Success(Work(URI.create(job.input), getProcessOutputDir(job), job))
+        }
+        case "s3" => {
+          val (bucket, key) = S3Utils.parse(input)
+          val file = key.split("/").last
+          val tempFile = Paths.get(baseTemp.getAbsolutePath, file)
+          try {
+            logger.info(s"Download ${input}")
+            val req = new GetObjectRequest(bucket, key)
+            val s3Object = s3.getObject(req)
+            Files.copy(s3Object.getObjectContent(), tempFile, REPLACE_EXISTING)
+            Success(Work(
+              tempFile.toUri,
+              URI.create(job.output),
+              job))
+          } catch {
+            case ase: AmazonServiceException => {
+              logger.error("Caught an AmazonServiceException, which means your request made it to Amazon S3, but was rejected with an error response for some reason.");
+              logger.error("Error Message:    " + ase.getMessage());
+              logger.error("HTTP Status Code: " + ase.getStatusCode());
+              logger.error("AWS Error Code:   " + ase.getErrorCode());
+              logger.error("Error Type:       " + ase.getErrorType());
+              logger.error("Request ID:       " + ase.getRequestId());
+              Failure(ase)
+            }
+            case ace: AmazonClientException => {
+              logger.error("Caught an AmazonClientException, which means the client encountered a serious internal problem while trying to communicate with S3, such as not being able to access the network.");
+              logger.error("Error Message: " + ace.getMessage())
+              Failure(ace)
+            }
+          }
+        }
+        case scheme =>
+          logger.warn(s"Input URI scheme ${scheme} not supported");
+          Failure(new ProcessorException(new IllegalArgumentException(s"Input URI scheme ${scheme} not supported"), job))
+      }
+    } catch {
+      case e: Exception =>
+        //        e.printStackTrace();
+        Failure(new ProcessorException(e, job))
+    }
+  }
+
+//  private def runDitaOt(jobTry: Try[Work]): Future[Try[Work]] = Future {
+//    logger.debug(s"Process: " + jobTry)
+//    jobTry match {
+//      case Success(work) => {
+//        try {
+//          logger.info(s"Running graphics conversion: " + work)
+//          val start = System.currentTimeMillis()
+//
+//          val file = new File(work.input)
+//          logger.info(s"Open r+w ${work.input}")
+//          logger.info("For each entry")
+//          logger.info("  If entry is PDF")
+//          logger.info("    Convert entry to PNG")
+//          logger.info("    Save PNG to ZIP")
+//          logger.info("Close ZIP")
+//
+//          val end = System.currentTimeMillis()
+//          logger.info(s"Process took ${format(end - start)}")
+//
+//          // FIXME
+//          val output = URI.create("s3://" + "xdoccont-data-prod" + "/" + "wnc/temp/" + file.getName)
+//
+//          val res = work.copy(
+//            task = work.task.copy(status = StatusString.Done),
+//            output = output
+//          )
+//          Success(res)
+//        } catch {
+//          case e: Throwable if getError(e).isDefined => {
+//            logger.debug("Error in runDitaOt: " + e.getMessage, e)
+//            stopHook.foreach(callback => callback())
+//            throw getError(e).get
+//          }
+//          case e: Exception => {
+//            val res = work.task.copy(status = StatusString.Error)
+//            Failure(new ProcessorException(e, res))
+//          }
+//        }
+//      }
+//      case f => f
+//    }
+//  }
+
+//  private def getError(e: Throwable): Option[java.lang.Error] = {
+//    if (e.isInstanceOf[java.lang.Error]) {
+//      return Some(e.asInstanceOf[java.lang.Error])
+//    }
+//    if (e.getCause != null) {
+//      return getError(e.getCause)
+//    }
+//    None
+//  }
+
+  def upload(tryTask: Try[Work]): Future[Try[Work]] = Future {
+    logger.debug(s"Upload: " + tryTask)
+    tryTask match {
+      case Success(work) => try {
+        work.output.getScheme match {
+          case "file" if !work.output.getPath.endsWith("/") => {
+            logger.info(s"Already generated output to ${work.output} directory")
+//            if (output != work.output) {
+//              throw new IllegalArgumentException(s"Output directory ${work.output} should match ${output}")
+//            }
+            Success(work)
+          }
+          case "s3" => {
+            val (bucket, key) = S3Utils.parse(work.output)
+            try {
+              logger.info(s"Upload ${work.input} to ${work.output}")
+              val req = new PutObjectRequest(bucket, key, new File(work.input))
+              s3.putObject(req)
+              Success(work)
+            } catch {
+              case ase: AmazonServiceException => {
+                logger.error("Caught an AmazonServiceException, which means your request made it to Amazon S3, but was rejected with an error response for some reason.");
+                logger.error("Error Message:    " + ase.getMessage());
+                logger.error("HTTP Status Code: " + ase.getStatusCode());
+                logger.error("AWS Error Code:   " + ase.getErrorCode());
+                logger.error("Error Type:       " + ase.getErrorType());
+                logger.error("Request ID:       " + ase.getRequestId());
+                Failure(ase)
+              }
+              case ace: AmazonClientException => {
+                logger.error("Caught an AmazonClientException, which means the client encountered a serious internal problem while trying to communicate with S3, such as not being able to access the network.");
+                logger.error("Error Message: " + ace.getMessage())
+                Failure(ace)
+              }
+            }
+          }
+          case _ =>
+            throw new IllegalArgumentException(s"Upload target ${work.output} not supported")
+        }
+      } catch {
+        case e: Exception =>
+          //          e.printStackTrace();
+          Failure(new ProcessorException(e, work.task))
+      }
+      case f => f
+    }
+  }
+
+  override def log(offset: Int): Seq[String] = List.empty
+
+  override def addStopHook(callback: () => Unit): Unit = {
+    stopHook = Some(callback)
+  }
+}
