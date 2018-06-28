a40ecc1fbb191158c65460b7b9aeccfea7e03aad
Jarno Elovirta <jarno@elovirta.com>
2018-06-28 16:31:55 +0300

    Add test for unzip

--- common/test/services/UtilsSpec.scala
+++ common/test/services/UtilsSpec.scala
@@ -1,10 +1,22 @@
 package services
 
+import java.io.{File, IOException}
 import java.net.URI
 
 import org.scalatest._
 
-class UtilsSpec extends FlatSpec with Matchers {
+class UtilsSpec extends FlatSpec with Matchers with BeforeAndAfter {
+
+  private val tempDir = File.createTempFile("temp", System.nanoTime.toString)
+
+  before {
+    if (tempDir.exists() && !tempDir.delete()) {
+      throw new IOException()
+    }
+    if (!tempDir.mkdirs()) {
+      throw new IOException()
+    }
+  }
 
   "parse" should "return file and path" in {
     val jarURI = URI.create("jar:s3://bucket/key/file.zip!/path/file.txt")
@@ -23,5 +35,27 @@ class UtilsSpec extends FlatSpec with Matchers {
     a [IllegalArgumentException] should be thrownBy {
       Utils.parse(jarURI)
     }
+  }
+
+  "unzip" should "extract all files" in {
+    val zip = new File(getClass.getResource("/test.zip").toURI)
+    Utils.unzip(zip, tempDir)
+
+    new File(tempDir, "file.txt").exists shouldBe true
+    new File(tempDir, "file.txt").isFile shouldBe true
+    new File(tempDir, "dir").exists() shouldBe true
+    new File(tempDir, "dir").isDirectory shouldBe true
+    new File(tempDir, "dir" + File.separator + "file.txt").exists shouldBe true
+    new File(tempDir, "dir" + File.separator + "file.txt").isFile shouldBe true
+  }
+
+  after {
+    def deleteWalker(file: File): Unit = {
+      if (file.isDirectory) {
+        file.listFiles.foreach(deleteWalker(_))
+      }
+      file.delete()
+    }
+    deleteWalker(tempDir)
   }
 }
