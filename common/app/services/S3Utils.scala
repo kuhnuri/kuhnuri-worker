6e867f768a09589d48edadea5a3c90e383986011
Jarno Elovirta <jarno@elovirta.com>
2018-06-29 07:25:37 +0300

    Improve S3 URI parsing

--- common/app/services/S3Utils.scala
+++ common/app/services/S3Utils.scala
@@ -5,6 +5,9 @@ import java.net.URI
 object S3Utils {
 
   def parse(uri: URI): (String, String) = {
+    if (uri.getScheme != "s3") {
+      throw new IllegalArgumentException(s"Invalid S3 URI: ${uri}")
+    }
     return (uri.getAuthority, uri.getPath.substring(1))
   }
 
