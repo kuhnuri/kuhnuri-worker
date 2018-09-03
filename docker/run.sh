fdcd5b329a64af81d833c9a85942afa6caedaf9d
Jarno Elovirta <jarno@elovirta.com>
2018-09-03 15:52:54 +0300

    Use exec in Docker script to fix signals

--- docker/run.sh
+++ docker/run.sh
@@ -10,4 +10,4 @@ source /opt/app/config/env.sh
 if [ -f /opt/app/RUNNING_PID ]; then
     rm /opt/app/RUNNING_PID
 fi
-java $JAVA_OPTS -Duser.dir=/opt/app -Dconfig.file=conf/$ENVIRONMENT.conf -cp $CLASSPATH play.core.server.ProdServerStart
+exec java $JAVA_OPTS -Duser.dir=/opt/app -Dconfig.file=conf/$ENVIRONMENT.conf -cp $CLASSPATH play.core.server.ProdServerStart
